790ad9c53b874625528a3d2479956a0370be6dc3
vibui97 <41174423+vibui97@users.noreply.github.com>
2018-07-13 14:46:56 +0700

    Update helper.js

--- helper.js
+++ helper.js
@@ -19,5 +19,19 @@ module.exports={
 			}
 		}
 		return missParam;
+	},
+	encrypt: function(key, data) {
+        var cipher = crypto.createCipher('aes-256-cbc', key);
+        var crypted = cipher.update(data, 'utf-8', 'hex');
+        crypted += cipher.final('hex');
+
+        return crypted;
+	},
+
+	descrypt: function(key, data) {
+        var decipher = crypto.createDecipher('aes-256-cbc', key);
+        var decrypted = decipher.update(data, 'hex', 'utf-8');
+        decrypted += decipher.final('utf-8');
+        return decrypted;
 	}
 }
